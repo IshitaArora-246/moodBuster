b9b619e4f6fcd9b482313b0e3be732173d5bba85
Ishita <ishita_arora_@outlook.com>
2021-04-19 23:47:25 +0530

    constants added

--- lib/constants/textStyle.dart
+++ lib/constants/textStyle.dart
@@ -1,8 +1,35 @@
 import 'package:flutter/material.dart';
 
-final headingStyle =
-    TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black);
+final headingStyle = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.bold,
+  color: Colors.black,
+);
 
 final paraStyle = TextStyle(
-    fontSize: 18, fontWeight: FontWeight.normal, color: Colors.grey[900]);
-    
+  fontSize: 18,
+  fontWeight: FontWeight.normal,
+  color: Colors.grey[900],
+);
+
+class CustomText extends StatelessWidget {
+  final String text;
+  final double size;
+  final Color color;
+  final FontWeight weight;
+
+  // name constructor that has a positional parameters with the text required
+  // and the other parameters optional
+  CustomText({@required this.text, this.size, this.color, this.weight});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+          fontSize: size ?? 16,
+          color: color ?? Colors.black,
+          fontWeight: weight ?? FontWeight.normal),
+    );
+  }
+}
