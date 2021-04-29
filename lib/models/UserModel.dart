55bea158b88aa23b4670a822691d22d2c3c77de2
Ishita <ishita_arora_@outlook.com>
2021-04-29 23:02:34 +0530

    removed extra lines

--- lib/models/UserModel.dart
+++ lib/models/UserModel.dart
@@ -1,6 +1,3 @@
-import 'package:meta/meta.dart';
-
-@immutable
 class UserModel {
   UserModel({
     this.doc,
--- lib/models/blog.dart
+++ lib/models/blog.dart
@@ -1,9 +1,9 @@
-class Blog{
+class BlogModel {
   final String title;
   final String content;
   final String author;
   final String imageUrl;
   final String source;
 
-  Blog(this.title, this.content, this.author, this.imageUrl, this.source);
+  BlogModel(this.title, this.content, this.author, this.imageUrl, this.source);
 }
